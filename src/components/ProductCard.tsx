79802279ff16b6e54d2705bed1c584aa01885ff7
RubAv4 <ruberthjavalosgamez@gmail.com>
2025-08-10 19:56:30 -0500

    feat: modal con galería + sección Sobre mí

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import type { Producto } from "../types";
 
 type Props = {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import ProductModal from "../components/ProductModal";
 import type { Producto } from "../types";
